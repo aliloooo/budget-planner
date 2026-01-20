e7e9c639827e89954d80dcbeb6182b8e329d559c
aidp <alridp@aidp-2.local>
2026-01-20 10:29:26 +0700

    Fix invalid import path in Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from 'react-router-dom'
-import { useAuth } from '../../context/AuthContext'
+import { useAuth } from '../context/AuthContext'
 import Sidebar from './layout/Sidebar'
 import BottomNav from './layout/BottomNav'
 
